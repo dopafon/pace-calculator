1f1f71435f9b2511c2ffcb50a5c2fbe280f96476
Marco Schroeder <marco.schroeder1@1und1.de>
2020-09-08 18:08:53 +0200

    provide option to show splits or not

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,11 @@
 from flask import flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
+from wtforms import StringField, SubmitField, IntegerField, BooleanField
 from wtforms.validators import DataRequired, Optional, InputRequired
   
 class CalcForm(FlaskForm):
     swim_pace_time = StringField('Your swim pace per 100m', validators=[Optional()], description='HH:MM:SS')
     swim_race_time = StringField('Your swim time (total)', validators=[Optional()], description='HH:MM:SS')
     distance = IntegerField('Race distance in metres', validators=[DataRequired()])
+    showsplits = BooleanField('Show splits')
     submit = SubmitField('Calculate')
--- app/routes.py
+++ app/routes.py
@@ -9,8 +9,7 @@ def validate_pace_or_time():
         flash('At least one of race or pace time must be set', 'error')
     elif form.swim_pace_time.data and form.swim_race_time.data:
         flash('Please specify ONLY "pace" OR "time"', 'error')
-        return render_template('swim.html', title='Swim', form=form)
-
+        
 @app.route('/', methods=['GET'])
 def homepage():
     return redirect('/swim')
@@ -25,23 +24,35 @@ def swim():
             pace_time_str = form.swim_pace_time.data
             pace_secs = get_secs_from_time(pace_time_str)
             swimtime = get_time_by_pace(pace_secs, form.distance.data)
-            swimsplits = create_swimsplits(get_secs_from_time(swimtime), form.distance.data)            
+            if form.showsplits.data is True:
+                swimsplits = create_swimsplits(get_secs_from_time(swimtime), form.distance.data)            
+                showsplits = True
+            else:
+                swimsplits = ''
+                showsplits = False
             return render_template('swim.html', title='Swim',
                 form=form,
                 swimtime=swimtime,
                 swimpace=convert_secs_to_time(pace_secs),
-                swimsplits=swimsplits)
+                swimsplits=swimsplits,
+                showsplits=showsplits)
         elif form.swim_race_time.data is not None:
             if form.swim_race_time.data != '':
                 race_time_str = form.swim_race_time.data
                 race_time_secs = get_secs_from_time(race_time_str)
                 pacetime = get_swimpace_by_time(form.distance.data, race_time_secs)
-                swimsplits = create_swimsplits(race_time_secs, form.distance.data)
-                validate_pace_or_time()
+                if form.showsplits.data is True:
+                    swimsplits = swimsplits = create_swimsplits(race_time_secs, form.distance.data)
+                    showsplits = True
+                else:
+                    swimsplits = ''
+                    showsplits = False
                 return render_template('swim.html', title='Swim', 
                     form=form,
                     swimtime=convert_secs_to_time(race_time_secs),
-                    swimpace=pacetime, swimsplits=swimsplits)
+                    swimpace=pacetime,
+                    swimsplits=swimsplits,
+                    showsplits=showsplits)
             else:
                 return render_template('swim.html', title='Swim', form=form)    
         else:
